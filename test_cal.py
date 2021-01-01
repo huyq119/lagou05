0b59dfde79f27ce1f8e895261a6f968b21822a02
huyq119 <yuqi.hu@gmail.com>
2021-01-01 21:29:46 +0800

    胡玉琦的作业，更新第二版

--- test_cal.py
+++ test_cal.py
@@ -1,4 +1,6 @@
 import pytest
+import yaml
+
 from pythoncode.calculator import Calculator
 
 
@@ -6,6 +8,15 @@ class TestCalc:
     def setup_class(self):
         # 实例化类,生成类的对象
         self.calc = Calculator()
+
+    def teardown_class(self):
+        print("\n所有用例完成")
+
+    def setup_method(self):
+        print("\n开始计算")
+
+    def teardown_method(self):
+        print("\n结束计算")
 
     #  使用参数化
     @pytest.mark.parametrize("a,b,expect", [(3, 5, 8), (-1, -2, -3), (10000, 10000, 20000)])
@@ -16,8 +27,28 @@ class TestCalc:
         # 判断result结果是否等于期望的值
         assert result == expect
 
+    @pytest.mark.parametrize("a, b, expect", yaml.safe_load(open("./sub_param.yml")))
+    # 测试sub函数
     def test_sub(self, a, b, expect):
-        # 调用sub函数，返回的记过保存在result里面
+        # 调用sub函数，返回的结果保存在result里面
+        print("减法函数")
         result = self.calc.sub(a, b)
+        # 判断result结果是否等于期望的值
+        assert result == expect
+
+    @pytest.mark.parametrize("a, b, expect", yaml.safe_load(open("./mul_param.yml")))
+    # 测试mul函数
+    def test_mul(self, a, b, expect):
+        # 调用mul函数，返回的结果保存在result里面
+        print("乘法函数")
+        result = self.calc.mul(a, b)
+        # 判断result结果是否等于期望的值
+        assert result == expect
+
+    @pytest.mark.parametrize("a, b, expect", yaml.safe_load(open("./div_param.yml")))
+    def test_div(self, a, b, expect):
+        # 调用mul函数，返回的结果保存在result里面
+        print("除法函数")
+        result = self.calc.div(a, b)
         # 判断result结果是否等于期望的值
         assert result == expect
